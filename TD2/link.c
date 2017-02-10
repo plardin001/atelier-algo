76fe65670fe0a350dd4134c35c20b6046dea6950
hmahtout <hmahtout@enseirb-matmeca.fr>
2017-02-10 16:52:11 +0100

    Glas du wk

--- TD2/link.c
+++ TD2/link.c
@@ -2,14 +2,21 @@
 
 void				lnk__empty(struct link *a)
 {
-	if (a == NULL || a->head == NULL)
+	struct lelement *sentinel;
+	
+	if (a == NULL)
 		return ;
-	a->head->next = a->head;
+	sentinel = (struct lelement *)malloc(sizeof(struct lelement));
+	if (sentinel == NULL)
+		return ;
+	sentinel->data = -1;
+	sentinel->next = sentinel;
+	a->head = sentinel;
 }
 
 struct lelement		*lnk__first(struct link *a)
 {
-	if (a == NULL)
+	if (a == NULL || a->head == NULL)
 		return (NULL);
 	return (a->head);
 }
@@ -24,33 +31,29 @@ int					llm__is_end_mark(struct lelement *is_sentinel)
 
 void				lnk__add_head(struct link *a, struct lelement *new)
 {
-	struct lelement *head;
-	
 	if (a == NULL || a->head == NULL || new == NULL)
 		return ;
 	new->next = a->head;
 	a->head = new;
+	return ;
 }
 
 struct lelement		*lnk__remove_head(struct link *a)
 {
-	struct lelement	*head;
 	struct lelement	*next;
 	struct lelement	*del;
 
-	if (a == NULL || a->head == NULL)
+	if (a == NULL || a->head == NULL || llm__is_end_mark(a->head))
 		return (NULL);
 	del = a->head;
-	a->head->next = next;
+	next = a->head->next;
 	a->head = next;
 	return (del);
 }
 
 struct lelement		*llm__next(struct lelement *a)
 {
-	if (a == NULL)
-		return (NULL);
-	if (a->next == a)
+	if (a == NULL || llm__is_end_mark(a->next))
 		return (a);
 	else
 		return (a->next);
@@ -59,59 +62,49 @@ struct lelement		*llm__next(struct lelement *a)
 struct lelement		*lnk__add_after(struct link *lst, struct lelement *begin, struct lelement *new)
 {
 	struct lelement *tmp;
-	struct lelement *next;
 
-	if (lst == NULL || lst->head == NULL || begin == NULL || begin->next == begin || new == NULL)
+	if (lst == NULL || lst->head == NULL || begin == NULL || new == NULL || llm__is_end_mark(begin))
 		return (NULL);
-	next = begin->next;
+	tmp = begin->next;
 	begin->next = new;
-	new->next = next;
+	new->next = tmp;
 	return (new);
 }
 
-struct lelement		*lnk__remove_after(struct link *lst, struct lelement *begin, struct lelement *new)
+struct lelement		*lnk__remove_after(struct link *lst, struct lelement *begin)
 {
 	struct lelement *del;
 	struct lelement *next;
-	struct lelement *tmp;
 
-	if (lst == NULL || lst->head == NULL || begin == NULL || begin->next == begin || new == NULL)
+	if (lst == NULL || lst->head == NULL || begin == NULL || llm__is_end_mark(begin))
 		return (NULL);
-	tmp = lst->head;
-	while (tmp != begin)
-	{
-		if (tmp->next == tmp)
-			return (NULL);
-		tmp = tmp->next;
-	}
 	del = begin->next;
 	next = del->next;
-	begin ->next = next;
+	begin->next = next;
 	return (del);
 }
 
 void			lnk__add_tail(struct link *lst, struct lelement *new)
 {
-	struct lelement *del;
-	struct lelement *next;
+	struct lelement *sentinel;
 	struct lelement *tmp;
 
 	if (lst == NULL || lst->head == NULL || new == NULL)
 		return ;
 	tmp = lst->head;
-	if (tmp->next = tmp)
+	if (llm__is_end_mark(tmp))
 	{
 		lst->head = new;
 		new->next = tmp;
 		return ;
 	}
-	while (tmp->next->next != tmp->next)
+	while (!llm__is_end_mark(tmp->next))
 	{
 		tmp = tmp->next;
 	}
-	next = tmp->next;
+	sentinel = tmp->next;
 	tmp->next = new;
-	new->next = tmp;
+	new->next = sentinel;
 }
 
 struct lelement			*lnk__remove_tail(struct link *lst)
@@ -120,16 +113,16 @@ struct lelement			*lnk__remove_tail(struct link *lst)
 	struct lelement *sentinel;
 	struct lelement *tmp;
 
-	if (lst == NULL || lst->head == NULL || new == NULL || lst->head->next == lst->head )
+	if (lst == NULL || lst->head == NULL || llm__is_end_mark(lst->head))
 		return (NULL);
 	tmp = lst->head;
-	if (tmp->next->next = tmp->next)
+	if (llm__is_end_mark(tmp->next))
 	{
-		del = tmp->next;
-		lst->head->next = lst->head;
+		del = tmp;
+		lst->head = tmp->next;
 		return (del);
 	}
-	while (tmp->next->next->next != tmp->next->next)
+	while (!llm__is_end_mark(tmp->next->next))
 	{
 		tmp = tmp->next;
 	}
